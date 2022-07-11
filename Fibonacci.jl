9dc1bea97aa1a47dabce86c3f77b9a7f6bbbd6cb
tyrashark <58677260+tyrashark@users.noreply.github.com>
2022-07-11 16:12:42 +0900

    Create Fibonacci.jl

---
+++ Fibonacci.jl
@@ -0,0 +1,13 @@
+function F(n)
+    if n <= 0
+        return 1
+    elseif n <= 1
+        return 1
+    else
+        return F(n-1) + F(n-2)
+    end
+end
+
+function F1(n)
+    return n <= 0 ? 1 : (n <= 1 ? 1 : F1(n-1) + F(n-2))
+end
