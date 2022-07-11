7c55e7a79e98685d6d413d3e5099eb9152836b7a
tyrashark <58677260+tyrashark@users.noreply.github.com>
2022-07-11 16:27:51 +0900

    Update Kleinfourgroups.jl

--- Kleinfourgroups.jl
+++ Kleinfourgroups.jl
@@ -2,29 +2,21 @@ struct Klein
     explicit::Char
 end
 
+## overload some base functions (+, inv, *)
+
 import Base.+
-
-
 function +(x::String, y::String)
     return x * y
 end
 
-
-isidentity(x::Klein) = x.explicit ∈ ['e']
-
-e = Klein('e'); 가 = Klein('e')
-a = Klein('a'); 나 = Klein('a')
-b = Klein('b'); 다 = Klein('b')
-c = Klein('c'); 라 = Klein('c')
-
 import Base.inv
-
 function inv(x::Klein)
     return x
 end
 
 import Base.*
 function *(x::Klein, y::Klein)
+    isidentity(x::Klein) = x.explicit ∈ ['e']
     if isidentity(y)
         return x
     elseif isidentity(x)
@@ -35,3 +27,9 @@ function *(x::Klein, y::Klein)
         return setdiff(Set([a,b,c]), Set([x,y]))
     end
 end
+
+e = Klein('e'); 가 = Klein('e')
+a = Klein('a'); 나 = Klein('a')
+b = Klein('b'); 다 = Klein('b')
+c = Klein('c'); 라 = Klein('c')   
+ 
