1ad3bb1ca8fa0498d156d3958878a8b5fe76004a
tyrashark <58677260+tyrashark@users.noreply.github.com>
2022-08-25 16:57:30 +0900

    Update Fibonacci2.jl

--- Fibonacci2.jl
+++ Fibonacci2.jl
@@ -1,14 +1,20 @@
-function F1(n)
-    return n <= 0 ? 1 : (n <= 1 ? 1 : F1(n-1) + F(n-2))
+function fibo1(n)
+    x = []
+    function F1(n)
+        return n <= 0 ? 1 : (n <= 1 ? 1 : F1(n-1) + F1(n-2))
+    end
+    while length(x) <= n 
+     push!(x, F1(length(x)))
+    end
+    return reverse(x)
 end
 
-function FA(n)  
-    push!(x, F1(n))
-    return n <= 0 ? x : FA(n-1)
-end
-
-function FA2(n)
-    j = length(x)
-    j <= 0 ? push!(x, 1) : (j <= 1 ? push!(x, 1) : push!(x, x[length(x)] + x[length(x)-1]))
-    return n <= j ? reverse(x) : FA2(n)
+function fibo2(n)
+    x = [] 
+    function FA2(n)
+        j = length(x)
+        j <= 0 ? push!(x, 1) : (j <= 1 ? push!(x, 1) : push!(x, x[length(x)] + x[length(x)-1]))
+        return n <= j ? reverse(x) : FA2(n)
+    end
+return FA2(n)
 end
